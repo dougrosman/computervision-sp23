d0f46ede707ead18455d182cc993a66dd0861a02
Doug Rosman <dougrosman@gmail.com>
2023-01-29 17:51:01 -0600

    text rain works

--- examples/artworks/text-rain/sketch.js
+++ examples/artworks/text-rain/sketch.js
@@ -3,7 +3,7 @@
 const cam_w = 640;
 const cam_h = 480;
 let capture;
-const phrase = "please catch me"
+const phrase = "please catch me, i'm falling down!"
 let fallingLetters = [];
 
 function setup() {
@@ -12,34 +12,39 @@ function setup() {
     capture.size(cam_w, cam_h);
 
     for(let i = 0; i < phrase.length; i++) {
+        const fontSize = 20;
         const letter = phrase[i];
-        const position = createVector(i * 14, 0);
-        fallingLetters.push(new FallingLetter(letter, position));
+        const position = createVector(i * fontSize, 0);
+        fallingLetters.push(new FallingLetter(letter, position, fontSize));
     }
 }
 
 function draw() {
     clear(); // clear the canvas before drawing content
-    const threshold = map(mouseX, 0, width, 0, 255);
 
     capture.loadPixels();
     
     fallingLetters.forEach(fallingLetter => {
 
-       while(!checkBrightnessAtPixel(fallingLetter.position)) {
+       while(fallingLetter.position.y > 0 &&
+            fallingLetter.position.y < height &&
+            !checkBrightnessAtPixel(fallingLetter)) {
         fallingLetter.position.y--;
        }
        fallingLetter.update();
+
+       fallingLetter.draw();
     })
 }
 
 // if bright, keeping falling. if dark, go up
 // should return true if bright, false if dark
-function checkBrightnessAtPixel(position) {
+function checkBrightnessAtPixel(fl) {
 
-    const threshold = 100;
+    const threshold = floor(map(mouseX, 0, width, 0, 256));
+    const mirrorX = width-fl.position.x
 
-    const index = (position.x + position.y * cam_w ) * 4;
+    const index = (mirrorX + fl.position.y * cam_w) * 4;
     const r = capture.pixels[index]
     const g = capture.pixels[index+1]
     const b = capture.pixels[index+2]
@@ -47,27 +52,33 @@ function checkBrightnessAtPixel(position) {
     // calculate the pixel brightness by finding the average of the three channels
     const brightness = floor((r + g + b) / 3)
 
+    // keep falling
     if(brightness > threshold) {
         return true;
     }
-
+    // stop falling
     return false;    
 }
 
 class FallingLetter {
-    constructor(letter, position) {
+    constructor(letter, position, fontSize) {
         this.letter = letter;
         this.position = position;
+        this.fallRate = 2;
+        this.fontSize = fontSize;
     }
 
     update() {
-        this.position.y++;
-        if(this.position.y > height) {
+        if(this.position.y > height+20) {
             this.position.y = 0;
         }
+        this.position.y+=this.fallRate;;
+        
     }
 
     draw() {
+        fill(255);
+        textSize(this.fontSize);
         text(this.letter, this.position.x, this.position.y);
     }
 }
