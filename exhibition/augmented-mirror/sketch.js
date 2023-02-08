0193d09c114a66dcb2eade9f0c39b98e31e971e7
Doug Rosman <dougrosman@gmail.com>
2023-02-08 05:48:19 -0600

    streamline capture setting

--- exhibition/augmented-mirror/sketch.js
+++ exhibition/augmented-mirror/sketch.js
@@ -4,9 +4,10 @@ const cam0_w = 640;
 const cam0_h = 360;
 
 const cam1_w = 640;
-const cam1_h = 640;
+const cam1_h = 480;
 
-let currentCapture = 0;
+const cam2_w = 640;
+const cam2_h = 640;
 
 // 16:9
 let capture0;
@@ -14,32 +15,70 @@ let capture0;
 // 1:1
 let capture1;
 
-let mode = 0;
+let mode = 1;
 const numModes = 5;
 let autoToggle = false;
 let toggleTime = 2000;
 
 let dTime = 0;
+
+/////// Emris
+let weatherData;
+let apiKey = "87fb783a54817f1793f0556477730e7c";
+// chicago
+let lat = "41.878113";
+let lon = "-87.629799";
+let h;
+let currentH;
+let currentWeather;
+let unixDT;
+let unixSunrise;
+let unixSunset;
+let bgVal;
+
+
+function preload() {
+    let url =
+        "https://api.openweathermap.org/data/2.5/weather?lat=41.878113&lon=-87.629799&appid=87fb783a54817f1793f0556477730e7c";
+    weatherData = loadJSON(url);
+}
+
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    pixelDensity(1);
+
     capture0 = createCapture(VIDEO);
-    capture1 = createCapture(VIDEO);
     capture0.size(cam0_w, cam0_h);
-    capture1.size(cam1_w, cam1_h);
     capture0.hide();
+
+    capture1 = createCapture(VIDEO);
+    capture1.size(cam1_w, cam1_h);
     capture1.hide();
+
+    capture2 = createCapture(VIDEO);
+    capture2.size(cam2_w, cam2_h);
+    capture2.hide();
+
+
+    /////// Emris
+    parseData();
+
+    bgVal = map(minute(), 0, 60, 0, 255);
+    console.log(bgVal);
+    h = hour();
+    currentH = h;
 }
 
 function draw() {
-    //clear();
 
     switch (mode) {
         case 0:
             mirrorMeadow();
             break;
-        // case 1:
-        //     mirrorDoug1();
-        //     break;
+        case 1:
+            mirrorEmris();
+            break;
         // case 2:
         //     mirrorDoug2();
         //     break;
@@ -66,29 +105,96 @@ function draw() {
 }
 
 
-function mirrorMeadow() {
-    currentCapture = 2;
-    capture1.loadPixels();
 
-    const scaleRatio = windowWidth / capture1.width;
-    const scaledHeight = capture1.height * scaleRatio;
+
+function mirrorEmris() {
+    
+    let capture = setCapture(1);
+
+    background(bgVal/1.5, 0, bgVal, 10);
+    const scaleRatio = min(windowWidth / capture.width, windowHeight / capture.height);
+    const scaledHeight = capture.height * scaleRatio;
+    const scaledWidth = capture.width * scaleRatio;
 
     push()
-    translate(0, (windowHeight - scaledHeight) / 2);
+    translate((windowWidth - scaledWidth) / 2, (windowHeight - scaledHeight) / 2);
     scale(scaleRatio, scaleRatio);
 
-    if (capture1.pixels.length > 0) {
+    h = hour();
+    if (currentH != h) {
+        parseData();
+        currentH = h;
+    }
+    if ((unixDT > unixSunrise) && (unixDT < unixSunset)) {
+        fill('yellow');
+        ellipse(capture.width / 5, capture.height / 5, 30, 30);
+    } else {
+        fill('white');
+        ellipse(capture.width / 5, capture.height / 5, 20, 20);
+    }
 
+    capture.loadPixels();
+    if (capture.pixels.length > 0) {
 
         const stepSize = 10;
 
-        for (let y = stepSize / 2; y < capture1.height; y += stepSize) {
-            for (let x = stepSize / 2; x < capture1.width; x += stepSize) {
-                const index = (capture1.width - x + y * capture1.width) * 4;
+        for (let y = stepSize / 2; y < capture.height; y += stepSize) {
+            for (let x = stepSize / 2; x < capture.width; x += stepSize) {
+                const index = (capture.width - x + y * capture.width) * 4;
 
-                const r = capture1.pixels[index];
-                const g = capture1.pixels[index + 1];
-                const b = capture1.pixels[index + 2];
+                const r = capture.pixels[index];
+                const g = capture.pixels[index + 1];
+                const b = capture.pixels[index + 2];
+                const brightness = (10 + g + b) / 2.4;
+
+                if (x % 3 == 0) {
+                    fill(bgVal, 255-bgVal, 255-bgVal, 255 - brightness*3);
+                    textSize(5);
+                    text(currentWeather, x-(bgVal/2), y-(bgVal/2));
+                  }
+            
+                  const size = map(brightness, 0, 255, stepSize, stepSize * 3);
+                  noStroke();
+                  fill((r + b) / 2, 0, b, 255 - brightness * 2);
+            
+                  ellipse(x, y, size * 4, size / 3);
+            }
+        }
+    }
+    pop();
+}
+
+function parseData() {
+    currentWeather = weatherData.weather[0].description;
+    unixDT = weatherData.dt;
+    unixSunrise = weatherData.sunrise;
+    unixSunset = weatherData.sunset;
+}
+
+function mirrorMeadow() {
+    let capture = setCapture(2);
+
+    background(255);
+    const scaleRatio = min(windowWidth / capture.width, windowHeight / capture.height);
+    const scaledHeight = capture.height * scaleRatio;
+    const scaledWidth = capture.width * scaleRatio;
+    
+    push()
+    translate((windowWidth - scaledWidth) / 2, (windowHeight - scaledHeight) / 2);
+    scale(scaleRatio, scaleRatio);
+    
+    capture.loadPixels();
+    if (capture.pixels.length > 0) {
+
+        const stepSize = 10;
+
+        for (let y = stepSize / 2; y < capture.height; y += stepSize) {
+            for (let x = stepSize / 2; x < capture.width; x += stepSize) {
+                const index = (capture.width - x + y * capture.width) * 4;
+
+                const r = capture.pixels[index];
+                const g = capture.pixels[index + 1];
+                const b = capture.pixels[index + 2];
                 const brightness = (10 + g + b) / 2.4;
 
                 noStroke();
@@ -107,16 +213,19 @@ function mirrorMeadow() {
     pop();
 }
 
+
+
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 
-    if(currentCapture === 1) {
+    if (currentCapture === 1) {
         displayScaler = width / capture1.width;
     }
     displayScaler = width / capture0.width;
 }
 
 function keyPressed() {
+    clear();
     switch (keyCode) {
         case RIGHT_ARROW:
             mode++;
@@ -164,4 +273,21 @@ function keyPressed() {
 function mousePressed() {
     let fs = fullscreen();
     fullscreen(!fs);
+}
+
+function setCapture(num) {
+    let capture;
+    switch (num) {
+        case 0:
+            capture = capture0;
+            break;
+        case 1:
+            capture = capture1;
+            break;
+        case 2:
+            capture = capture2;
+            break;
+    }
+
+    return capture;
 }
