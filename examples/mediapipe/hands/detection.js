96cf800cf5900bb1e90ba99924230dbeb4fc4251
Doug Rosman <dougrosman@gmail.com>
2023-02-14 13:20:44 -0600

    hands working

--- examples/mediapipe/hands/detection.js
+++ examples/mediapipe/hands/detection.js
@@ -9,9 +9,7 @@ function gotHands(results) {
 }
 
 const hands = new Hands({locateFile: (file) => {
-  
-  console.log(`https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`)
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+  return `models/hands/${file}`;
 }});
 hands.setOptions({
   maxNumHands: 2,
