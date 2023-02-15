a392f7af291df4448a9a08d71181e11648304a9b
Doug Rosman <dougrosman@gmail.com>
2023-02-15 05:05:53 -0600

    finish face detections

--- examples/mediapipe/face-detection/sketch.js
+++ examples/mediapipe/face-detection/sketch.js
@@ -3,47 +3,39 @@ let sketch = function(p) {
 
   p.setup = function() {
     p.createCanvas(cam_w, cam_h);
+    p.rectMode(p.CENTER);
   }
 
   p.draw = function() {
     p.clear(0);
-
-    console.log(detections);
-
+    
     if(detections != undefined) {
       if(detections.detections != undefined) {
-        
         p.drawFaces();
+        // console.log(detections.detections);
       }
     }
   }
 
   p.drawFaces = function() {
-    p.stroke(0);
     p.strokeWeight(8);
 
     for(let i = 0; i < detections.detections.length; i++) {
+
+      // it's not necessary to create this boundingBox variable, but it makes for less typing and neater coder
+      const boundingBox = detections.detections[i].boundingBox;
+      p.noStroke();
+      p.fill(255, 0, 255, 80);
+      p.rect(p.width-boundingBox.xCenter*p.width, boundingBox.yCenter*p.height, boundingBox.width * p.width, boundingBox.height * p.height);
+
+      p.stroke(0, 255, 0);
       for(let j = 0; j < detections.detections[i].landmarks.length; j++) {
-
-        const x = p.width - (detections.detections[i][j].x * p.width)
-        const y = (detections.detections[i][j].y * p.height)
-
-        console.log(x, y);
+        const facePoint = detections.detections[i].landmarks[j]
+        const x = p.width - (facePoint.x * p.width)
+        const y = facePoint.y * p.height
         p.point(x, y);
       }
     }
-    
-    // for(let i = 0; i < detections.multiHandLandmarks.length; i++) {
-    //   for(let j = 0; j < detections.multiHandLandmarks[i].length; j++) {
-    //     const x = p.width-(detections.multiHandLandmarks[i][j].x * p.width);
-    //     const y = detections.multiHandLandmarks[i][j].y * p.height;
-    //     const z = detections.multiHandLandmarks[i][j].z;
-     
-    //     p.point(x, y, z);
-         
-        
-    //   }
-    // }
   }
 }
 
