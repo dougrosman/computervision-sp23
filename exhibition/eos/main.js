f3c07b1bd28e0599fdf8c8d8d1d4b22d67e2f286
Doug Rosman <dougrosman@gmail.com>
2023-05-03 23:46:41 -0500

    finger to boundingbox works

--- exhibition/eos/main.js
+++ exhibition/eos/main.js
@@ -1,96 +1,143 @@
-let sketch = function(p) {
+let links = document.getElementsByClassName("work-link");
 
-    let font;
-  
-    p.preload = function() {
-      font = p.loadFont("Poppins-Medium.ttf");
-    }
-  
-    p.setup = function() {
-      p.createCanvas(cam_w, cam_h, p.WEBGL);
-      p.setAttributes({alpha: true})
-      p.textFont(font);
-      p.textSize(16)
-    }
-  
-    p.draw = function() {
-      p.clear(0);
-      p.translate(-p.width/2, -p.height/2);
-  
-      if(detections != undefined) {
-        if(detections.multiHandLandmarks != undefined) {
-          p.drawHands();
-        }
+let sketch = function (p) {
+  let font;
+
+  p.preload = function () {
+    font = p.loadFont("Poppins-Medium.ttf");
+  };
+
+  p.setup = function () {
+    p.createCanvas(cam_w, cam_h, p.WEBGL);
+    p.setAttributes({ alpha: true });
+    p.textFont(font);
+    p.textSize(12);
+    p.pixelDensity(1);
+  };
+
+  p.draw = function () {
+    p.clear(0);
+    p.translate(-p.width / 2, -p.height / 2);
+
+    if (detections != undefined) {
+      if (detections.multiHandLandmarks != undefined) {
+        p.drawHands();
       }
     }
-  
-    p.drawHands = function() {
+  };
+
+  p.drawHands = function () {
     //   p.stroke(0);
-      p.strokeWeight(4);
-  
+    p.strokeWeight(4);
+
+    for (let i = 0; i < detections.multiHandLandmarks.length; i++) {
+      const THUMB_TIP = detections.multiHandLandmarks[0][4];
+
+      const THUMB = p.createVector(
+        p.width - THUMB_TIP.x * p.width,
+        THUMB_TIP.y * p.height,
+        THUMB_TIP.z
+      );
+
+      const INDEX_FINGER_TIP = detections.multiHandLandmarks[0][8];
+
+      const INDEX = p.createVector(
+        p.width - INDEX_FINGER_TIP.x * p.width,
+        INDEX_FINGER_TIP.y * p.height,
+        INDEX_FINGER_TIP.z
+      );
+
+      p.point(INDEX.x, INDEX.y, INDEX.z);
+      p.point(THUMB.x, THUMB.y, THUMB.z);
+      const FINGER_DISTANCE = THUMB.dist(INDEX);
+
       
-  
-      for(let i = 0; i < detections.multiHandLandmarks.length; i++) {
-        
-        const THUMB_TIP = detections.multiHandLandmarks[0][4];
+      
 
-        const THUMB = p.createVector(
-                                p.width-(THUMB_TIP.x * p.width),
-                                THUMB_TIP.y * p.height,
-                                THUMB_TIP.z
-                                );
+      if (FINGER_DISTANCE < 20) {
+        p.stroke(0, 255, 0);
 
-        const INDEX_FINGER_TIP = detections.multiHandLandmarks[0][8];
+        const selectRatioX = p.windowWidth / cam_w;
+        const selectRatioY = p.windowHeight / cam_h;
+        
+        const selectPointVector = THUMB.lerp(INDEX, 0.5);
+        const selectPoint = {
+          x: selectPointVector.x * selectRatioX,
+          y: selectPointVector.y * selectRatioY
+        };
+        p.point(selectPointVector.x, selectPointVector.y, selectPointVector.z);
+        p.text(`${Math.floor(selectPoint.x)}, ${Math.floor(selectPoint.y)}`, selectPointVector.x, selectPointVector.y, selectPointVector.z);
 
-        const INDEX = p.createVector(
-                                        p.width-(INDEX_FINGER_TIP.x * p.width),
-                                        INDEX_FINGER_TIP.y * p.height,
-                                        INDEX_FINGER_TIP.z
-                                        );
-        
-        p.point(INDEX.x, INDEX.y, INDEX.z);
-        p.point(THUMB.x, THUMB.y, THUMB.z);
-        const SELECT_POINT = THUMB.lerp(INDEX, 0.5);
-        p.point(SELECT_POINT.x, SELECT_POINT.y, SELECT_POINT.z);
-
-        const FINGER_DISTANCE = THUMB.dist(INDEX);
-
-        
-        if(FINGER_DISTANCE < 15) {
-            p.stroke(0, 255, 0);
-        } else {
-            p.stroke(255, 0, 0);
-        }
-
-  
-        // for(let j = 0; j < detections.multiHandLandmarks[i].length; j++) {
-  
-        //   const x = p.width-(detections.multiHandLandmarks[i][j].x * p.width);
-        //   const y = detections.multiHandLandmarks[i][j].y * p.height;
-        //   const z = detections.multiHandLandmarks[i][j].z;
-          
-        //   p.point(x, y, z);
-          
-  
-        //   p.text(i, x, y, z);
-  
-  
-        //   // calculate how far the hand is from the camera by calculating the distance between keypoints 9 and 13 (base of middle and ring finger)
-        //   if(j == 9 || j == 13) {
-        //     const palm1 = detections.multiHandLandmarks[0][9]
-        //     const palm2 = detections.multiHandLandmarks[0][13]
-        
-        //     const palm1Pos = p.createVector(palm1.x, palm1.y);
-        //     const palm2Pos = p.createVector(palm2.x, palm2.y);
-        
-        //     const distance = palm1Pos.dist(palm2Pos)
-        
-        //     // console.log(distance);
-        //   }
-           
-        // }
+        fingerSelect(selectPoint);
+      } else {
+        p.stroke(255, 0, 0);
+        fingerDeSelect();
       }
     }
+  };
+};
+
+let myp5 = new p5(sketch);
+
+
+
+function fingerSelect(selectPoint) {
+  links.forEach((link) => {
+
+    const linkBox = link.getBoundingClientRect();
+
+    if (
+      selectPoint.x > linkBox.x &&
+      selectPoint.x < linkBox.x + linkBox.width &&
+      selectPoint.y > linkBox.y &&
+      selectPoint.y < linkBox.y + linkBox.height
+    ) {
+      console.log(linkBox);
+      console.log(selectPoint);
+      link.style.border = "4px solid rgb(0, 255, 0)";
+    } else {
+        link.style.border = "none";
+    }
+
+    // check if mouse position is in bounding box
+    // if(mousePos.x)
+  });
+}
+
+function fingerDeSelect() {
+    links.forEach((link) => {
+      link.style.border = "none";
+    });
   }
-  
-  let myp5 = new p5(sketch)
+
+
+
+// //mouse debug
+//   window.addEventListener("mousemove", (e) => {
+
+//     const mousePos = { x: e.clientX, y: e.clientY };
+//     // console.log(mousePos)
+
+//     links.forEach((link) => {
+
+//         // console.log(link.getBoundingClientRect());
+
+//         const linkBox = link.getBoundingClientRect();
+
+//         if(mousePos.x > linkBox.x &&
+//            mousePos.x < linkBox.x + linkBox.width &&
+//            mousePos.y > linkBox.y &&
+//            mousePos.y < linkBox.y + linkBox.height
+//         ) {
+//             console.log(link.innerHTML);
+//             link.style.border = '4px solid rgb(0, 255, 0)';
+//         } else {
+//             link.style.border = 'none';
+//         }
+
+//         // check if mouse position is in bounding box
+//         // if(mousePos.x)
+
+//     })
+
+// })
