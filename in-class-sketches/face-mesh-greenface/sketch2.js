a8ca1376dfe6779429d06af29d4659383552229c
Doug Rosman <dougrosman@gmail.com>
2023-10-25 15:18:14 -0500

    update

---
+++ in-class-sketches/face-mesh-greenface/sketch2.js
@@ -0,0 +1,19 @@
+const UPPER_LEFT = [234, 119, 6, 10, 67, 54];
+const UPPER_RIGHT = [];
+const MIDDLE_LEFT = [];
+const MIDDLE_RIGHT = [];
+const LOWER_LEFT = [];
+const LOWER_RIGHT = [];
+
+const UPPER_LEFT_MIDPOINT = 434;
+
+// const ALL_FACE_REGIONS = [UPPER_LEFT, UPPER_RIGHT, LOWER_LEFT, LOWER_RIGHT]
+
+
+p.beginShape()
+for(let i = 0; i < UPPER_LEFT.length; i++) {
+
+// add all the vertex points to the shape
+
+}
+p.endShape();
